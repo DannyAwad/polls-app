b96df2c2c98bfa6001cc9460c15882552b04415d
Mohamed Awde <116069156+MohamedAwde@users.noreply.github.com>
2023-07-05 16:39:17 +0200

    ng-bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'my-app';
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
 }
